c9e2ce0843ba1cfe359d175b8116981e05280d58
Seliutina <iryna.korotych@gmail.com>
2016-05-30 01:33:47 +0300

    Module 2-2 changes.

---
+++ src/Module02/File/Flowers/BaseFlowersSet.java
@@ -0,0 +1,11 @@
+package Module02.File.Flowers;
+
+import java.util.List;
+
+/**
+ * Created by Irina on 30.05.2016.
+ */
+public abstract class BaseFlowersSet {
+    List<Bouquet> Flowers;
+    List<Rosebust> Rosebust;
+}
--- src/Module02/File/Flowers/Rosebust.java
+++ src/Module02/File/Flowers/Rosebust.java
@@ -1,10 +1,8 @@
 package Module02.File.Flowers;
 
-import java.util.List;
 
 /**
  * Created by Irina on 30.05.2016.
  */
 public class Rosebust {
-    List<Bouquet> Roses;
 }
