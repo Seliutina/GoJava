5cd35b3df8222074572e33893fd4c7a9ceeab13e
Seliutina <iryna.korotych@gmail.com>
2016-05-29 23:07:55 +0300

    Initial commit of the project.

---
+++ src/Module01/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+package Module01;
+
+/**
+ * Created by Irina on 29.05.2016.
+ */
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hi! I'm your first fava program! Atay tuned!");
+    }
+}
