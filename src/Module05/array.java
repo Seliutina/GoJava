5d2333dc2358741755496ac890561efe2f588d5d
Seliutina <iryna.korotych@gmail.com>
2016-06-09 01:03:28 +0300

    Module5 home task.

---
+++ src/Module05/array.java
@@ -0,0 +1,14 @@
+package Module05;
+
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+     int[] array1 =  {28, 1031, 597, 472, 120, -50, 21, 865, 198, 67, -250, 624, 12, 452, 3};
+        Arrays.sort(array1);
+
+    System.out.println("Minimal element of array1 equals " + array1 [0]);
+        System.out.println("Maximal element of array1 equals " + array1 [14]);
+
+    }
+}
